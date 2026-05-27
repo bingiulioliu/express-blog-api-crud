711b3a4f3dafa357185198140bf454aaf1e39e07
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-27 15:30:46 +0200

    FEAT collegato middleware di ricerca slug a show, patch e delete

---
+++ middlewares/findSlug.js
@@ -0,0 +1,25 @@
+import posts from "../data/posts.js";
+
+function findSlug (request, response, next) {
+    // Estraggo il parametro
+    const {slug} = request.params;
+
+    const postFind = posts.find(post => {
+        post.slug === slug;
+    });
+
+    // Gestione dell'errore
+    if (!postFind){
+        return response.status(404).json({
+            error: 'Post non trovato',
+            results: null
+        });
+    }
+
+    // Passaggio di dati e continuazione
+    request.postFind = postFind;
+    next();
+
+};
+
+export default findSlug;
--- routers/posts.js
+++ routers/posts.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import {index, show, create, destroy, modify} from '../controllers/posts.js'
-
+import findSlug from '../middlewares/findSlug.js'
 const router = express.Router();
 
 // INDEX hhtp://localhost:5555/posts
 router.get('/', index)
 
 // SHOW hhtp://localhost:5555/posts/1
-router.get('/:slug', show);
+router.get('/:slug',findSlug, show);
 
 // CREATE
 router.post('/', create)
 
 // PATCH
-router.patch('/:slug', modify)
+router.patch('/:slug',findSlug, modify)
 
 // DELETE
-router.delete('/:slug', destroy)
+router.delete('/:slug', findSlug, destroy)
 
 
 export default router
